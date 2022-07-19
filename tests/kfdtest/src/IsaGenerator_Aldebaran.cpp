ca7c93da2b01ef19112ee1828fba7c1212b56196
Wen-Heng (Jack) Chung <whchung@gmail.com>
2022-07-19 16:56:06 +0000

    Add s_nop to cater for s_cbranch instructions.

--- tests/kfdtest/src/IsaGenerator_Aldebaran.cpp
+++ tests/kfdtest/src/IsaGenerator_Aldebaran.cpp
@@ -144,6 +144,7 @@ const uint32_t IsaGenerator_Aldbrn::GEMM_ISA_16_1152_5120[] = {
 0xBE8D0003,             //     s_mov_b32 s13, s3
 0xBE8E0004,             //     s_mov_b32 s14, s4
 0xBE8F0005,             //     s_mov_b32_s15, s5
+0xBF800000,             // s_nop 0
 
 // The actual GEMM algorithm from  Microsoft based on CK.
 0x8E038407,             // s_lshl_b32 s3, s7, 4                                             // 000000000014: 
@@ -702,6 +703,7 @@ const uint32_t IsaGenerator_Aldbrn::GEMM_ISA_16_1280_5120[] = {
 0xBE8D0003,             //     s_mov_b32 s13, s3
 0xBE8E0004,             //     s_mov_b32 s14, s4
 0xBE8F0005,             //     s_mov_b32_s15, s5
+0xBF800000,             // s_nop 0
 
 // The actual GEMM algorithm from  Microsoft based on CK.
 0x8E038407,             // s_lshl_b32 s3, s7, 4                                             // 000000000014: 
@@ -986,6 +988,7 @@ const uint32_t IsaGenerator_Aldbrn::GEMM_ISA_16_5120_1280[] = {
 0xBE8D0003,             //     s_mov_b32 s13, s3
 0xBE8E0004,             //     s_mov_b32 s14, s4
 0xBE8F0005,             //     s_mov_b32_s15, s5
+0xBF800000,             // s_nop 0
 
 // The actual GEMM algorithm from  Microsoft based on CK.
 0x8E038407,             // s_lshl_b32 s3, s7, 4                                             // 000000000014: 
